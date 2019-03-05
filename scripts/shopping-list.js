4e6e0abb90c9ae5759623f5d7e4e634c515ad34e
Silas <silasishallahan@gmail.com>
2019-03-04 23:50:48 -0800

    delete is working

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -106,9 +106,12 @@ const shoppingList = (function(){
       // get the index of the item in store.items
       const id = getItemIdFromElement(event.currentTarget);
       // delete the item
-      store.findAndDelete(id);
-      // render the updated shopping list
-      render();
+      api.deleteItem(id)
+        .then(() => {
+          store.findAndDelete(id);
+          // render the updated shopping list
+          render();
+        });
     });
   }
   
